5d8c6254d512140ccb5687a89724bd37c2679c5d
Roshen Nair <roshen620@gmail.com>
2022-02-14 22:39:08 +0800

    Add validation methods for customers' phone numbers, email address and bank card numbers

--- src/main/java/com/mycompany/hostelhomeappliancesservicecentre/Customer.java
+++ src/main/java/com/mycompany/hostelhomeappliancesservicecentre/Customer.java
@@ -40,19 +40,19 @@ public class Customer extends Person {
 	this.bankCard = bankCard;
     }
 
-    // TODO: Implement phone number validation using RegExp
-    private static boolean validatePhoneNumber(String phoneNumber) {
-	return false;
+    // Returns true for any phone number that starts with +60 followed by 8-10 digits
+    private static boolean isValidPhoneNumber(String phoneNumber) {
+	return phoneNumber.matches("^\\+60[0-9]{8,10}$");
     }
 
-    // TODO: Implement email validation using RegExp
-    private static boolean validateEmail(String email) {
-	return false;
+    // Returns true for any email address that only contains letters, numbers, dashes, underscores and dots
+    private static boolean isValidEmail(String email) {
+	return email.matches("^[A-Z0-9+_.-]+@[A-Z0-9.-]+$");
     }
 
-    // TODO: Implement bank card validation using RegExp
-    private static boolean validateBankCard(String bankCard) {
-	return false;
+    // Returns true for any bank card number that contains 8-19 digits
+    private static boolean isValidBankCard(String bankCard) {
+	return bankCard.matches("^[0-9]{8,19}$");
     }
 
     private static Customer parse(String customerLine) {
